5c91d223b3c87958c5ad59ca1587615bd9d51406
jg054098 <jacob.grieger@cerner.com>
2018-09-19 08:20:29 +0200

    submission two added

---
+++ Two/Two/HmacSha512Hasher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Two
+{
+    /// <summary>
+    /// cerner_2^5_2018
+    /// </summary>
+    public sealed class HmacSha512Hasher
+    {
+        public string ComputeBase64Hash(byte[] hashKey, string inputToHash)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(inputToHash);
+
+            using (var hashAlgorithm = new HMACSHA512(hashKey))
+            {
+                var hash = hashAlgorithm.ComputeHash(inputBytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
+    }
+}
